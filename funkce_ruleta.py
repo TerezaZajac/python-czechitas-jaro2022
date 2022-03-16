21c4804fe3eb443b0d6cfd8dd097383cb8155bd1
Tereza Z <zajacova.tereza@gmail.com>
2022-03-16 20:26:17 +0100

    update validace vstupu

--- funkce_ruleta.py
+++ funkce_ruleta.py
@@ -1,10 +1,15 @@
 import random 
 
 def roulette(row, bet):
+    # if row > 3 or row < 1:
+    #     return 'neplatna rada'
+    if not (1<= row and row <= 3):
+        return 'neplatna rada'
+
     x = random.randint(0,36)
-    print(x)
+    #print(x)
     if x == 0:
-        return - bet
+        return 0
     elif x % 3 == 1 and row == 1:
         return bet * 2
     elif x % 3 == 2 and row == 2:
@@ -12,8 +17,9 @@ def roulette(row, bet):
     elif x % 3 == 0 and row == 3:
         return bet * 2
     else:
-        return - bet
+        return 0
     
 row = input('Zvol si jednu ze tri rad: ')
 bet = input('Vyska tve sazky: ')
+
 print(roulette(int(row), int(bet)))
