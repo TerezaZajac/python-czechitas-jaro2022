3f63f1c29e17fd733fa1f39ff2fa89edaf88a317
Tereza Z <zajacova.tereza@gmail.com>
2022-03-16 23:29:14 +0100

    03: smska: finalni verze s bonusem

--- 03SMS_brana.py
+++ 03SMS_brana.py
@@ -12,37 +12,21 @@
 # Nápověda
 # Pokud chcete zkontrolovat předvolbu, stačí využít podmínku"+420 in cislo, alternativně můžete využít indexy: Python umožňuje kromě jednoho znaku z řetězce získat i více znaků, a to pomocí dvojtečky. Pokud budete chtít získat první čtyři znaky, napište cislo[0:4]. Pak můžete vytvořit podmínku cislo[0:4] == "+420".
 
+
+def phone_num_length(number):
+    return len(number) == 9 or len(number) == 13
+
+def message_price(message):
+    return 3 * (int(len(message) / 180) + 1)
+
 phone_number = input('Zadej sve telefoni cislo: ')
-phone_message = ''
-def phone_num_length():
-    if len(phone_number) == 9 or len(phone_number) == 13:
-        return True
-    else: 
-        return False
 
-if phone_num_length() == True:
-    phone_message = input('Zadej svoji zpravu: ')
-else:
+if not phone_num_length(phone_number.replace(' ','')):
     print('spatne cislo')
+    exit(0)
 
-#phone_message = input('Zadej svoji zpravu: ')
-#print(phone_number)
-#print(len(phone_number))
+phone_message = input('Zadej svoji zpravu: ')
 
-print(phone_message)
+print('Cena vasi sms je', message_price(phone_message), 'kc')
 
-price = 0
-def message_price():
-    if 0 <= len(phone_message) <= 180:
-        price += 3
-    elif 180 < len(phone_message) >= 360:
-        price += 6
-    elif 360 < len(phone_message) >= 540:
-        price += 9
-    elif 540 < len(phone_message) >= 720:
-        price += 12
-    else:
-        print('zprava je moc dlouha')
-    return price
-print(message_price())
 
